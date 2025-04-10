cc82e86bcfaad3a3c5bc43436caa8f16b1e4c2fd
Hanzala Khan <46111633+hanzalakhan010@users.noreply.github.com>
2025-04-10 07:03:07 +0000

    recursion

--- dist/recursion.js
+++ dist/recursion.js
@@ -25,12 +25,15 @@ const { isReturnStatement } = require("typescript");
 //         b = temp
 //     }
 // }
-// function fib(n){
-//     if (n == 0) return 0
-//     if (n==1)return 1
-//     return fib(n-2) +fib(n-1)
-// }
-// console.log(fib(10))
+function fib(n){
+    if (n == 0) return 0
+    if (n==1)return 1
+    console.log(n)
+    return fib(n-2) +fib(n-1)
+}
+console.log(fib(10))
+
+
 
 // function parent(a,b){
 //     c = 2
@@ -54,12 +57,12 @@ const { isReturnStatement } = require("typescript");
 // console.log(result1);
 // console.log(result2);
 
-const multiply = (multiplier) => (n) => multiplier * n;
+// const multiply = (multiplier) => (n) => multiplier * n;
 
-const double = multiply(2);
-const triple = multiply(3);
+// const double = multiply(2);
+// const triple = multiply(3);
 
-console.log(double(10));
-console.log(triple(10));
+// console.log(double(10));
+// console.log(triple(10));
 
 
