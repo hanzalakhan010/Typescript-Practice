dbb9970259eb1d023ba976aaf2fbf9ce111e2291
Hanzala Khan <pythondeveloper360@gmail.com>
2025-04-12 10:11:22 +0500

    Something new

--- dist/Pick peaks.js
+++ dist/Pick peaks.js
@@ -1,31 +1,26 @@
 "use strict";
 function pickPeaks(arr) {
-    let pos = [];
-    let peaks = [];
-    // arr = arr.reverse();
-    if (arr) {
-        for (let i = 1; i < arr.length - 1; i++) {
-            if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1]) {
-                pos.push(i);
-                peaks.push(arr[i]);
-            }
-            else if (arr[i - 1] < arr[i] && arr[i] == arr[i + 1]) {
-                pos.push(i);
-                peaks.push(arr[i]);
-            }
-            else if (arr[i - 1] == arr[i] && arr[i] < arr[i + 1]) {
-                pos.push(i);
-                peaks.push(arr[i]);
-            }
-        }
+  let pos = [];
+  let peaks = [];
+  // arr = arr.reverse();
+  if (arr) {
+    for (let i = 1; i < arr.length - 1; i++) {
+      if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1]) {
+        pos.push(i);
+        peaks.push(arr[i]);
+      }
+      else if (arr[i-1]<arr[i] && arr[i] == arr[i+1]){
+        pos.push(i);
+        peaks.push(arr[i]);
+        console.log(i)
+      }
     }
-    return { pos, peaks };
+  }
+  return { pos, peaks };
 }
 // console.log(pickPeaks([3,2,3,6,4,1,2,3,2,1,2,2,2,1]))
-console.log(pickPeaks([
-    -2, 6, 2, -2, 0, 12, -2, 15, 15, 9, 10,
-    -2, -3, -3, -4, 3, 3, 12, 9, 2, -1, 8,
-    4, 11, 1, 6, 12, 6, 4, -3, 7, -4, 0,
-    3, 7, -3, 13, 0, 10, -2, 5, -3, 13, 9,
-    -3, 14, 14
-]));
+console.log(
+  pickPeaks([
+    1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3,
+  ])
+);
--- src/Pick peaks.ts
+++ src/Pick peaks.ts
@@ -2,20 +2,20 @@ function pickPeaks(arr: number[]): object {
   let pos: number[] = []
   let peaks: number[] = []
   // arr = arr.reverse();
+  let increasing = undefined
   if (arr) {
-    for (let i = 1; i < arr.length - 1; i++) {
-      if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1]) {
-        pos.push(i)
-        peaks.push(arr[i])
+    let pos_temp = 0
+    let peak_temp = 0
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i]<arr[i+1] && increasing){
+        increasing = false
+        pos.push(pos_temp)
+        peaks.push(peak_temp)
       }
-      else if (arr[i - 1] < arr[i] && arr[i] == arr[i + 1]) {
-
-        pos.push(i)
-        peaks.push(arr[i])
-      } else if (arr[i - 1] == arr[i] && arr[i] < arr[i + 1]) {
-        pos.push(i)
-        peaks.push(arr[i])
-
+      else if (arr[i]>arr[i+1] && !increasing){
+        increasing  = true
+        pos_temp = i
+        peak_temp = arr[i]
       }
     }
   }
@@ -23,10 +23,4 @@ function pickPeaks(arr: number[]): object {
 }
 
 // console.log(pickPeaks([3,2,3,6,4,1,2,3,2,1,2,2,2,1]))
-console.log(pickPeaks([
-  -2, 6, 2, -2, 0, 12, -2, 15, 15, 9, 10,
-  -2, -3, -3, -4, 3, 3, 12, 9, 2, -1, 8,
-  4, 11, 1, 6, 12, 6, 4, -3, 7, -4, 0,
-  3, 7, -3, 13, 0, 10, -2, 5, -3, 13, 9,
-  -3, 14, 14
-]))
+console.log(pickPeaks([1,2,5,4,3,2,3,6,4,1,2,3,3,4,5,3,2,1,2,3,5,5,4,3]))
