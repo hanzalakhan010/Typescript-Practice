7b89c2c3d48e991be356f1fb369adda382be9f48
Hanzala Khan <pythondeveloper360@gmail.com>
2025-04-14 18:38:29 +0500

    Refactor pickPeaks function and add new recursive functions for string manipulation and nested sum exploration

--- dist/Pick peaks.js
+++ dist/Pick peaks.js
@@ -1,26 +1,26 @@
 "use strict";
 function pickPeaks(arr) {
-  let pos = [];
-  let peaks = [];
-  // arr = arr.reverse();
-  if (arr) {
-    for (let i = 1; i < arr.length - 1; i++) {
-      if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1]) {
-        pos.push(i);
-        peaks.push(arr[i]);
-      }
-      else if (arr[i-1]<arr[i] && arr[i] == arr[i+1]){
-        pos.push(i);
-        peaks.push(arr[i]);
-        console.log(i)
-      }
+    let pos = [];
+    let peaks = [];
+    // arr = arr.reverse();
+    let increasing = undefined;
+    if (arr) {
+        let pos_temp = 0;
+        let peak_temp = 0;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] < arr[i + 1] && increasing) {
+                increasing = false;
+                pos.push(pos_temp);
+                peaks.push(peak_temp);
+            }
+            else if (arr[i] > arr[i + 1] && !increasing) {
+                increasing = true;
+                pos_temp = i;
+                peak_temp = arr[i];
+            }
+        }
     }
-  }
-  return { pos, peaks };
+    return { pos, peaks };
 }
 // console.log(pickPeaks([3,2,3,6,4,1,2,3,2,1,2,2,2,1]))
-console.log(
-  pickPeaks([
-    1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3,
-  ])
-);
+console.log(pickPeaks([1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3]));
--- src/recursion.ts
+++ src/recursion.ts
@@ -1,16 +1,17 @@
-"use strict";
 // function printNumber(n){
 //     if(n==0) return
 //     console.log(n)
 //     printNumber(n-1)
 // }
 // printNumber(10)
+
 // function factorial(n){
 //     if (n == 0) return 1
 //     if (n<0)return
 //     return n * factorial(n-1)
 // }
 // console.log(factorial(3))
+
 // function fib(n){
 //     a = 1
 //     b = 1
@@ -29,58 +30,68 @@
 //     return fib(n-2) +fib(n-1)
 // }
 // console.log(fib(10))
+
 // function parent(a,b){
 //     c = 2
 //     return ()=>{
 //         console.log(a+b)
 //     }
 // }
+
 // function createGreeting(greeting) {
 //   return function (name) {
 //     return `${greeting} ${name}`;
 //   };
 // }
+
 // const sayHi = createGreeting("Hi");
 // const sayHello = createGreeting("Hello");
+
 // const result1 = sayHi("Hanzala");
 // const result2 = sayHello("Hammad");
+
 // console.log(result1);
 // console.log(result2);
+
 // const multiply = (multiplier) => (n) => multiplier * n;
+
 // const double = multiply(2);
 // const triple = multiply(3);
+
 // console.log(double(10));
 // console.log(triple(10));
-const The_Mirror_Maze = (str) => {
-    if (str.length <= 1)
-        return str;
-    return The_Mirror_Maze(str.slice(1)) + str[0];
+
+const The_Mirror_Maze = (str: string): string => {
+  if (str.length <= 1) return str;
+  return The_Mirror_Maze(str.slice(1)) + str[0];
 };
 // console.log(The_Mirror_Maze("code"));
-const Fractal_Word_Builder = (word, n) => {
-    if (n <= 1)
-        return word;
-    return `cat[${Fractal_Word_Builder(word, n - 1)}]cat`;
+
+const Fractal_Word_Builder = (word: string, n: number): string => {
+  if (n <= 1) return word;
+  return `cat[${Fractal_Word_Builder(word, n - 1)}]cat`;
 };
+
 // console.log(Fractal_Word_Builder('cat',3))
-const Nested_Sum_Explorer = (element) => {
-    if (typeof element == "number")
-        return element;
-    if (Array.isArray(element) && element.length == 0)
-        return 0;
-    if (Array.isArray(element) && element.length > 0) {
-        let total = 0;
-        for (let ele of element) {
-            if (Array.isArray(ele)) {
-                total += Nested_Sum_Explorer(ele);
-            }
-            else {
-                total += ele;
-            }
-        }
-        return total;
+
+const Nested_Sum_Explorer = (element: any[]): number => {
+  if (typeof element == "number") return element;
+  if (Array.isArray(element) && element.length == 0) return 0;
+  if (Array.isArray(element) && element.length > 0) {
+    let total = 0;
+    for (let ele of element) {
+      if (Array.isArray(ele)) {
+        total += Nested_Sum_Explorer(ele);
+      }
+      else{
+
+          total += ele;
+      }
     }
-    return 0;
+    return total;
+  }
+  return 0
 };
+
 // console.log(Nested_Sum_Explorer([1, [1, [1]], 1]));
-const The_Tower_of_Emotions = (emotions) => { };
+const The_Tower_of_Emotions = (emotions :string[])=>{}
