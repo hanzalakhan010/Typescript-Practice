ca1b0e3d8effe8754e19635fddebbdb6ea840de6
Hanzala Khan <46111633+hanzalakhan010@users.noreply.github.com>
2025-04-15 05:43:58 +0000

    recursion

--- dist/recursion.js
+++ dist/recursion.js
@@ -52,35 +52,41 @@
 // console.log(double(10));
 // console.log(triple(10));
 const The_Mirror_Maze = (str) => {
-    if (str.length <= 1)
-        return str;
-    return The_Mirror_Maze(str.slice(1)) + str[0];
+  if (str.length <= 1) return str;
+  return The_Mirror_Maze(str.slice(1)) + str[0];
 };
 // console.log(The_Mirror_Maze("code"));
 const Fractal_Word_Builder = (word, n) => {
-    if (n <= 1)
-        return word;
-    return `cat[${Fractal_Word_Builder(word, n - 1)}]cat`;
+  if (n <= 1) return word;
+  return `cat[${Fractal_Word_Builder(word, n - 1)}]cat`;
 };
 // console.log(Fractal_Word_Builder('cat',3))
 const Nested_Sum_Explorer = (element) => {
-    if (typeof element == "number")
-        return element;
-    if (Array.isArray(element) && element.length == 0)
-        return 0;
-    if (Array.isArray(element) && element.length > 0) {
-        let total = 0;
-        for (let ele of element) {
-            if (Array.isArray(ele)) {
-                total += Nested_Sum_Explorer(ele);
-            }
-            else {
-                total += ele;
-            }
-        }
-        return total;
+  if (typeof element == "number") return element;
+  if (Array.isArray(element) && element.length == 0) return 0;
+  if (Array.isArray(element) && element.length > 0) {
+    let total = 0;
+    for (let ele of element) {
+      if (Array.isArray(ele)) {
+        total += Nested_Sum_Explorer(ele);
+      } else {
+        total += ele;
+      }
     }
-    return 0;
+    return total;
+  }
+  return 0;
 };
 // console.log(Nested_Sum_Explorer([1, [1, [1]], 1]));
-const The_Tower_of_Emotions = (emotions) => { };
+const The_Tower_of_Emotions = (emotions,tower) => {
+    if (emotions.length>0) {
+        return The_Tower_of_Emotions(emotions.slice(emotions.length-1))
+  }
+  return emotions;
+};
+
+let em = ['a','b','c']
+console.log(em.slice(0))
+console.log(em.slice(1))
+console.log(em.slice(2))
+// console.log(The_Tower_of_Emotions(["a", "b", "c"]));
