62db04367e80f9701811b313c5c866dddbcc1ead
Hanzala Khan <46111633+hanzalakhan010@users.noreply.github.com>
2025-04-23 01:30:34 +0000

    Add addProperty function and enhance getProperty usage in index.ts

---
+++ src/class3/index.ts
@@ -0,0 +1,55 @@
+let firstElement = <T>(arg: T[]): T => {
+  return arg[0];
+};
+// console.log(firstElement([1,2,3,4]))
+// console.log(firstElement(['a','b','c']))
+// console.log(firstElement([undefined,'b','c']))
+// console.log(firstElement([1,'b','c']))
+
+interface Person {
+  name: string;
+  age: number;
+}
+interface Address {
+  street: string;
+  city: string;
+}
+
+// // interface personDetails
+// function merge<P extends Person, A extends Address>(
+//   person: P,
+//   addr: A
+// ): Person & Address {
+//   return Object.assign(person, addr);
+//   //   return { ...person, ...addr };
+// }
+
+// console.log(
+//   merge({ name: "Hanzala", age: 19 }, { street: "xyx", city: "khi" })
+// );
+
+function getProperty<Type, Key extends keyof Type>(obj: Type, key: Key) {
+  return obj[key];
+}
+function addProperty<Type, Key extends string, Value>(
+  obj: Type,
+  key: Key,
+  value: Value
+) {
+  return { ...obj, [key]: value };
+}
+
+let x = { a: 1, b: 3, c: 2 };
+
+console.log("🚀 ~ getProperty:", getProperty(x, "a"));
+
+
+x = addProperty(x,'z',10)
+
+console.log(x)
+
+// console.log("🚀 ~ getProperty:", getProperty(x, "z"));
+
+
+
+
